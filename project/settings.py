db1b46950279e3de1ef107ef98cda519024fc03d
demo-23home <zeyadslama23@gmail.com>
2024-02-27 18:56:25 +0200

    Adding Jazzmin Dashboard

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -40,7 +41,7 @@ INSTALLED_APPS = [
     #Internal Apps
     "accounts",
     "doctor",
-    "patient"
+    "patient",
     #External Modules
     'rest_framework',
 
