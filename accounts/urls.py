cc1ee01cbcfca3b6448a39076b800299df904b24
demo-23home <zeyadslama23@gmail.com>
2024-02-27 20:55:46 +0200

    Adding DashBoard Function

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,10 +3,12 @@ from .views import (
     MyTokenObtainPairView,
     RegisterPatientView,
     RegisterDoctorView,
+    user_info,
 )
 
 urlpatterns = [
     path("token/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("register/patient/", RegisterPatientView.as_view(), name="register_patient"),
     path("register/doctor/", RegisterDoctorView.as_view(), name="register_doctor"),
+    path("user_info/", user_info),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -13,6 +13,12 @@ from .serializers import (
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import DoctorProfile, PatientProfile
+from .serializers import DoctorProfileSerializer, PatientProfileSerializer
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -35,3 +41,15 @@ class RegisterDoctorView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(role=User.Role.DOCTOR)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def user_info(request):
+    if request.user.role == "DOCTOR":
+        doctor_profile = DoctorProfile.objects.get(user=request.user)
+        serializer = DoctorProfileSerializer(doctor_profile)
+    else:
+        patient_profile = PatientProfile.objects.get(user=request.user)
+        serializer = PatientProfileSerializer(patient_profile)
+    return Response(serializer.data)
