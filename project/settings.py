32749c6c5c43408a4a0378e46389c11827521a47
demo-23home <zeyadslama23@gmail.com>
2024-03-08 02:45:33 +0200

    Fixing Deployment

--- project/settings.py
+++ project/settings.py
@@ -142,7 +142,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-WHITENOISE_MANIFEST_STRICT = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -178,6 +177,8 @@ SPECTACULAR_SETTINGS = {
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 django_heroku.settings(locals())
